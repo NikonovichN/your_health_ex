fc3048aab72aa90fbfe496db1e7941a30cdabdb2
Nikita Nikonovich <nikonovichnd@gmail.com>
2025-01-19 18:30:47 +0300

    chore: remove unnecessary code

---
+++ lib/src/your_health_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class YourHealthApp extends StatelessWidget {
+  const YourHealthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your Health',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Center'),
+        ),
+      ),
+    );
+  }
+}
